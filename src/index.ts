a5d26df06170d9d82a64af874649e00dfd3cf004
Juan Ferrer <juan.ferrer2@bbva.com>
2019-05-31 18:52:26 +0200

    change master address

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import {HancockEthereumClient, EthereumWallet} from '@hancock/sdk-nodejs';
 import {playWithTransfers} from './exercise1';
 import {playWithSmartContractAPI} from './exercise2';
-// import {playWithSmartContractAPI} from './exercise2';
 import {playWithTokenAPI} from './exercise3';
 
 export const options = {
-  privateKey: '0x6c47653f66ac9b733f3b8bf09ed3d300520b4d9c78711ba90162744f5906b1f8',
+  privateKey: '0x9837a2c2173f17a9a71bd43123de10d79cf6eba52ceb10f95b53a26462be86cf',
 };
 export const urlBase: string = 'https://s3-eu-west-1.amazonaws.com/archer-smartcontracts/eip20/ethereum/EIP20';
-export const masterWallet = '0x6c0a14f7561898b9ddc0c57652a53b2c6665443e';
+export const masterWallet = '0x441a03d4526171c0f5aac05345311be4fa78d039';
 export let walletA: EthereumWallet;
 
 const host: string = 'hancock-sandbox.kickstartteam.es';
